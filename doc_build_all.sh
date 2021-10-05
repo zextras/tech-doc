3034760283037d069445c06bf37f351e1b4b1453
root <root@dockerf>
2021-10-05 17:12:11 +0000

    added doc_build_all.sh

---
+++ doc_build_all.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+#
+# Simple script to build all products at once
+
+for i in carbonio enterprise landing suite; do
+    # use clean html to wipe out the old build and write a fresh one
+    make -C source/$i html
+done
+         
+echo""
+echo "Script finished"
