4db6954dcbc9ac1ecef102ec67a221e98f9795c4
Stefano David <code@stefanodavid.eu>
2021-10-14 10:16:10 +0200

    remove sphinx_panels extension

--- source/landing/conf.py
+++ source/landing/conf.py
@@ -30,7 +30,7 @@ root_doc = 'home'
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = [ 'sphinx_design', 'sphinx.ext.extlinks', 'sphinx_panels' ]
+extensions = [ 'sphinx_design', 'sphinx.ext.extlinks' ]
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
