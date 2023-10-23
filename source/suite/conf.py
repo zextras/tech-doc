477e4ab553469903ed143adff5c099e0e1a761f6
Stefano David <stefano.david@zextras.com>
2023-10-23 11:14:31 +0200

    [ZTD-562] remove duplicate fontawesome CSS (Suite)

--- source/suite/conf.py
+++ source/suite/conf.py
@@ -81,9 +81,7 @@ html_short_title = '%s Documentation' %project
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
-html_css_files = [ 'css/suite.css', 'css/common.css',
-                   'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/fontawesome.min.css'
-                  ]
+html_css_files = [ 'css/suite.css', 'css/common.css' ]
 html_logo = 'img/logos/suite-white.svg'
 
 html_theme_options= {
