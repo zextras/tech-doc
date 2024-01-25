c1026dce07079f16f088bd9684154b0fe51cb619
Stefano David <stefano.david@zextras.com>
2024-01-25 16:58:58 +0100

    [ZTD-630] update survey elements (ADV)

--- source/carbonio/conf.py
+++ source/carbonio/conf.py
@@ -129,8 +129,9 @@ html_sidebars = { "**": [ 'navbar-logo.html', 'survey', 'search-button-field.htm
 html_context = {
     'hubhome' : '%s' %hubhome,
     #change label and links - example values
-    'surveylink': 'https://app.useberry.com/t/jvNFeRHKKOxYSU/' ,
-    'surveylabel': 'Participate to our Documentation survey!',
+    'surveytext': 'What do you think about our new Documentation Layout?',
+    'surveylink': 'https://app.useberry.com/t/yXJRVr37I7Hmdu/?ver=24.1',
+    'surveylabel': 'Take the survey',
 }
 
 # workaround for ZTD-581
