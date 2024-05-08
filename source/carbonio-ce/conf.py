505b3e2422aa2b301daab7ff2a46ca7c1c759bc3
Stefano David <stefano.david@zextras.com>
2024-05-08 10:51:09 +0200

    [ZTD-696] remove matomo.js (CE)

--- source/carbonio-ce/conf.py
+++ source/carbonio-ce/conf.py
@@ -93,7 +93,7 @@ html_show_sourcelink = False
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
 html_css_files = [ 'css/community.css' ]
-html_js_files = [ 'js/matomo.js' ]
+html_js_files = [ 'js/posthog.js' ]
 html_favicon = 'img/favicon.ico'
 html_title = project + ' Documentation'
 html_last_updated_fmt = ''
