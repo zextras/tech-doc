4c725d76e93417099738eb7cc9e64f23d7e99388
Stefano David <stefano.david@zextras.com>
2024-12-23 16:45:06 +0100

    [ZTD-985] remove graphviz extension

--- source/carbonio/conf.py
+++ source/carbonio/conf.py
@@ -40,8 +40,7 @@ version = release
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [ 'sphinx_design', 'sphinx_copybutton',
-               'sphinx.ext.graphviz', 'sphinxcontrib.email',
-               'sphinx.ext.extlinks' ]
+               'sphinxcontrib.email', 'sphinx.ext.extlinks' ]
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
