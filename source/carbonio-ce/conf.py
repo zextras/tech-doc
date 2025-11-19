2870f082415b22abb583629f99436a26ff5bd664
Stefano David <stefano.david@zextras.com>
2025-11-19 17:37:44 +0100

    [ZTD-1528] bump versions (CE)

--- source/carbonio-ce/conf.py
+++ source/carbonio-ce/conf.py
@@ -30,11 +30,11 @@ copyright = '2025: ZEXTRAS'
 author = 'The Zextras Team'
 
 # this variable is used in the  upgrade procedure, remember to change it together with the release. Since upgrade doesn't discriminate on patch number, we do not need the last cipher.
-previous_release = '25.6'
+previous_release = '25.9'
 
 
 # The full version, including alpha/beta/rc tags
-release = '25.9.0'
+release = '25.12.0'
 version = release
 
 # -- General configuration ---------------------------------------------------
